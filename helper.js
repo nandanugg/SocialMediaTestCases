3aa8d7ea7fb291bbbf493980bbf32864509d0c5e
nandanugg <nandanugg@gmail.com>
2024-03-22 07:30:40 +0700

    asdf

--- helper.js
+++ helper.js
@@ -50,7 +50,7 @@ export function isEqual(v, query, expected) {
 }
 
 export function generateRandomImageUrl() {
-    return `${generateRandomDomain()}/image.png`
+    return `http://${generateRandomDomain()}/image.png`
 }
 
 export function generateRandomPhoneNumber(addPlusPrefix) {
